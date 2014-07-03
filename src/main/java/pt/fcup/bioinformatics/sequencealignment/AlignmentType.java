571964eb0c9ea27d1017088a6bb212dc7ae61f07
Arian Pasquali <arian@arianpasquali.com>
2014-07-03 23:47:50 +0100

    cost and alignment enum types

---
+++ src/main/java/pt/fcup/bioinformatics/sequencealignment/AlignmentType.java
@@ -0,0 +1,11 @@
+package pt.fcup.bioinformatics.sequencealignment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arian
+ * Date: 03/07/14
+ * Time: 22:39
+ */
+public enum AlignmentType {
+    LOCAL,GLOBAL
+}
---
+++ src/main/java/pt/fcup/bioinformatics/sequencealignment/costmatrix/MatrixCostType.java
@@ -0,0 +1,11 @@
+package pt.fcup.bioinformatics.sequencealignment.costmatrix;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arian
+ * Date: 03/07/14
+ * Time: 22:40
+ */
+public enum MatrixCostType {
+    NONE,PAM,BLOSUM
+}
